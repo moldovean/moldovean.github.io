2773f36c9c84bf264e346c2cc0c719d7d16a50f3
moldovean <adrian@vrabie.net>
2016-07-04 17:39:21 +0300

    changed the design, added day/night functionality

--- apps/weather/js/main.js
+++ apps/weather/js/main.js
@@ -1,7 +1,5 @@
 Geo = {};
 
-
-
 function errorPosition(){
   console.log("Could not get poisition");
   $("#location").html("Could not get poisition");
@@ -13,6 +11,7 @@ $(document).ready(function(){
   $("#getWeather").on("click",function(){location.reload();});
 
   $("h1").css('text-align','center');
+  //$("h3, h2, h1, p").css("color","white");
   $("*").css('text-align','center');
   //$("p").css('text-size','1.5em');
 
@@ -20,7 +19,7 @@ $(document).ready(function(){
     navigator.geolocation.getCurrentPosition(function(position){
       Geo.lat = position.coords.latitude;
       Geo.lng = position.coords.longitude;
-      $("#position").html("<i class='fa fa-crosshairs'></i> Lat: "+Math.round(Geo.lat*10000)/10000+ " Lng: "+ Math.round(Geo.lng*10000)/10000);
+      $("#position").html("<i class='fa fa-crosshairs'></i> Lat: "+Math.round(Geo.lat*1000)/1000+ " Lng: "+ Math.round(Geo.lng*1000)/1000);
       resolve(Geo);
     }, errorPosition);
   });
@@ -57,8 +56,26 @@ $(document).ready(function(){
           var icon = weatherIcons[code].icon;
 
           if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
-            if(data.dt>=data.sys.sunset) icon = 'night-' + icon;
-            else icon = 'day-' + icon;
+            if(data.dt>=data.sys.sunset){
+              icon = 'night-' + icon;
+              $("h3, h2, h1, p").css("color","white");
+              $(".TV").css("background","url('https://static.pexels.com/photos/36487/above-adventure-aerial-air.jpg')");
+              $(".main-site").css("background","linear-gradient(to bottom, rgb(12, 22, 38) 9%, rgb(116, 143, 180) 59%, rgb(42, 63, 103) 95%)");
+              $(".TV-info").css("background","#000000");
+
+
+            }
+            else {
+              icon = 'day-' + icon;
+              $("h3, h2, h1, p").css("color","black");
+              $(".TV").css("background","url('https://static.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg')");
+              $(".main-site").css("background","linear-gradient(179deg, rgb(182, 206, 246) 0%, rgb(67, 111, 171) 80%, rgb(103, 116, 141) 100%)");
+              $(".TV-info").css("background","#ffffff");
+
+              // testing .. delete in production
+              
+
+            }
           };
           icon = prefix + icon;
           skyHTML="<i class='"+icon+"'></i>"+skyHTML;
